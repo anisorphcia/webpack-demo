b6cc754446a5cb7db454b864b21667e2478aec4c
zdshen <zdshen@chinaums.com>
2024-07-01 20:16:04 +0800

    output

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,15 @@ module.exports = {
     // 没有设置的时候 entry 直接是根目录
     context: path.resolve(__dirname, './src'),
     // 文件入口
-    entry: './js/a.js',
+    entry: {
+        app: './js/a.js'
+    },
     // 打包后资源输出文件
     output: {
         // __dirname 当前文件的路径
         // path.resolve(__dirname, '') 当前文件夹根目录的绝对路径
         path: path.resolve(__dirname, ''),  // 输出路径
-        filename: 'bundle.js'   // 输出的文件名
+        filename: '[name]-bundle.js'   // 输出的文件名
     },
     module: {
         rules: [{
